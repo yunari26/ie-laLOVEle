23b472d637d7a6ebb6e517faad79534cd59bc672
dojiro-k <yujiro.koikeeeee@gmail.com>
2021-08-15 08:06:23 +0900

    selectページ調整

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.app')
 @section('content')
 <div id="images">
-    <a href="{{ url('/find?shop=1') }}">
-        <img src="images\omenya.png" class="image" id="image1">
-    </a>
-    <a href="{{ url('/find?shop=2') }}">
-        <img src="images\wanage.png" class="image" id="image2">
-    </a>
-    <a href="{{ url('/find?shop=3') }}">
-        <img src="images\sukui.png" class="image" id="image3">
-    </a>
+    <div class="up">
+        <div class="center">
+            <a href="{{ url('find?shop=1') }}">
+                <img src="images/omenya.png" class="image" id="image1">
+            </a>
+        </div>
+    </div>
+    <div class="down">
+        <div class="left">
+            <a href="{{ url('find?shop=2') }}">
+                <img src="images/wanage.png" class="image" id="image2">
+            </a>
+≈        </div>
+        <div class="right">
+            <a href="{{ url('find?shop=3') }}">
+                <img src="images/sukui.png" class="image" id="image3">
+            </a>
+        </div>
+    </div>
 </div>
 <style>
     #image1,
@@ -17,7 +27,10 @@
     #image3 {
         transition: all 1500ms 0s ease;
     }
-
+    body {
+        background-image: url(https://t4.ftcdn.net/jpg/02/76/07/57/360_F_276075762_iSnIlN6H6qhaoLJ59jMKLTEEtpqZ19la.jpg);
+        background-size: cover;
+    }
     .on {
         transform: scale(2.0, 2.0) translate(80px, 80px);
     }
@@ -26,7 +39,25 @@
         transform: scale(1.0, 1.0) translate(0px, 0px);
         opacity: 1;
     }
-
+    .up {
+        width: 100%;
+        margin-top: 200px;
+    }
+    .center {
+        text-align: center;
+    }
+    .left {
+        margin-left: 100px;
+    }
+    .right {
+        margin-right: 100px;
+    }
+    .down {
+        margin-top: 50px;
+        display: inline-block;
+        display: flex;
+        justify-content: space-between;
+    }
     .fadeout {
         animation: fadein-keyframes 2s ease 0.2s 1 forwards;
     }
